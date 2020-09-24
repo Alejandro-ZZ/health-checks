aae1d1c17f97b448239a16efb0d1f48ebabf019a
Alejandro Zambrano Zabaleta <alejozambranoz.97@gmail.com>
2020-09-24 13:15:26 -0500

    Add all_checks.py file

---
+++ all_checks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+import sys
+def check_reboot():
+    """Returns True id the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending reboot")
+        #exit program
+        sys.exist(1)
+    print("Everything ok.")
+    sys.exit(0)
+
+main()
